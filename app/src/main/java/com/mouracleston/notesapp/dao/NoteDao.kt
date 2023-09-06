9df7bb1f3d32f70958f72791dbebd60e0a55d73b
Cleston Moura <clestonmouractm@gmail.com>
2023-09-06 11:36:23 -0300

    Creates NoteDao with the main method fow writing and reading from database

---
+++ app/src/main/java/com/mouracleston/notesapp/dao/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.mouracleston.notesapp.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.mouracleston.notesapp.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Upsert
+    suspend fun upsertNote(note: Note)
+
+    @Query("SELECT * FROM note ORDER BY title ASC")
+    fun getNotesOrderedByTitle(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note ORDER BY createdAt ASC")
+    fun getNotesOrderedByCreatedAt(): Flow<List<Note>>
+
+}
